905d0a34e6903e0bedd498921406e92943dbf615
infinnitor <infinnitor@gmail.com>
2022-05-30 00:12:13 +0100

    Add combo_ticker UI

--- src/scoring.py
+++ src/scoring.py
@@ -1,9 +1,11 @@
 from sprite import *
-from constants import SCORE_TIMER
+from constants import SCORE_TIMER, TILE_SIZE
+
+import pygame.draw
 
 
 class ScoreBoard(Sprite):
-	LAYER = "MANAGER"
+	LAYER = "UI"
 
 	def __init__(self):
 		self._score = 0
@@ -21,7 +23,7 @@ class ScoreBoard(Sprite):
 	def update_move(self, game):
 		game.debug.display_text(f"SCORE: {self._score}")
 		game.debug.display_text(f"COMBO: {self._combo}")
-		game.debug.display_text(str(SCORE_TIMER - self._combo_ticker))
+		# game.debug.display_text(str(SCORE_TIMER - self._combo_ticker))
 
 		if game.sprites.PLAYER.destroy is True:
 			return
@@ -30,6 +32,12 @@ class ScoreBoard(Sprite):
 		if self._combo_ticker > SCORE_TIMER:
 			self._combo_ticker = 0
 			self._combo = 0
+
+	def update_draw(self, game):
+		x, y = game.sprites.GRID.relative_pos(game.sprites.PLAYER.pos(), center=False)
+
+		rat = 1 - (self._combo_ticker / SCORE_TIMER)
+		pygame.draw.rect(game.window, (195, 35, 35), [x, y+(TILE_SIZE-10), TILE_SIZE*rat, 10])
 
 	def get_score(self):
 		return self._score
