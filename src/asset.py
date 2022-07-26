7be8779f15b64ad09ca0c910a03e40f7f6c19ea8
infinnitor <infinnitor@gmail.com>
2022-07-26 01:29:10 +0100

    Fix error from asset.py

--- src/asset.py
+++ src/asset.py
@@ -7,24 +7,5 @@ from util.base import Namespace
 from visual_effects import mask_surface
 
 
-def _killsmap():
-	def _load(name):
-		file = pygame.image.load(f"data/sprites/vfx/killsmap/{name}").convert()
-		file.set_colorkey((0, 0, 0))
-		file = pygame.transform.scale(file, (TILE_SIZE, TILE_SIZE))
-		return mask_surface(file)
-
-	files = Namespace(
-		UP=_load("up.png"),
-		DOWN=_load("down.png"),
-		LEFT=_load("left.png"),
-		RIGHT=_load("right.png"),
-	)
-
-	global vfx_killsmap; vfx_killsmap = files
-
-
-
-
 def init_assets():
-	_killsmap()
+	pass
